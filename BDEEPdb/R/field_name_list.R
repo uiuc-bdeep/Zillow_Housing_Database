2b4e65598e914b1eb0e176ca1673e3c96e5681b6
Yuchen Liang <yuchenliang@wirelessprv-10-195-124-185.near.illinois.edu>
2018-07-24 16:31:04 -0500

    add column names

---
+++ BDEEPdb/R/field_name_list.R
@@ -0,0 +1,9 @@
+#' field_name
+#' @docType data
+#'
+#' @usage data(field_name)
+#'
+#' @format A string array that contains the column names in hedonics table
+#'
+#' @keywords hedonics
+"field_name"
