2d3bfd1afe7a8a13337ead0f1e813b5df11c67d4
Yuchen Liang <yuchenliang@wirelessprv-10-195-124-185.near.illinois.edu>
2018-07-24 10:28:51 -0500

    test data

--- BDEEPdb/R/database_R_data_transfer.R
+++ BDEEPdb/R/database_R_data_transfer.R
@@ -202,8 +202,7 @@ send_to_db <- function(df, table_name, schema_name="public", database_name="zill
 #'         all in lower case.
 #' @export
 get_state_county <- function(fips){
-  county_state_fips <- data(county_state_fips)
-  print(county_state_fips)
+  data(county_state_fips)
   ret <- setNames(data.frame(matrix(ncol = 2, nrow = 0)), c("state", "county"))
   for (n in fips){
     # Ensure correct format
