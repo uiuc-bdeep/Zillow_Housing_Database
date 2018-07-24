257f40d5062f61d78de343164196499ebd513cca
Yuchen Liang <yuchenliang@wirelessprv-10-195-124-185.near.illinois.edu>
2018-07-24 11:30:42 -0500

    test data 2

---
+++ BDEEPdb/R/county_state_fips_table.R
@@ -0,0 +1,12 @@
+#' county_state_fips
+#' @docType data
+#'
+#' @usage data(county_state_fips)
+#'
+#' @format A data.frame with 6 variables indicating state abbr., state name, county name,
+#'         state fips, county fips, and total fips
+#'
+#' @keywords fips
+#'
+#' @references \href{www.mdreducation.com/pdfs/US_FIPS_Codes.xls}
+"county_state_fips"
