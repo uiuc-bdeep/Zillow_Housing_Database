461cbbecc8ef8cc97a0fb012ea3e184190400b9b
Yuchen Liang <jacksonliang35@gmail.com>
2018-11-06 16:12:31 -0600

    update on infousa2018

--- BDEEPdb/R/database_R_data_transfer.R
+++ BDEEPdb/R/database_R_data_transfer.R
@@ -320,9 +320,9 @@ db_type_converter <- function(data, dbname = "zillow_2017_nov"){
       data$salespriceamount <- as.double(gsub("[\\$,]", "", data$salespriceamount))
   } else if (dbname == "infousa_2018"){
     # signaturedate
-    if("FAMILYID" %in% cols) data$FAMILYID <- as.character(data$FAMILYID)
+    if("FAMILYID" %in% cols) data$FAMILYID <- as.integer(data$FAMILYID)
     # signaturedate
-    if("LOCATIONID" %in% cols) data$LOCATIONID <- as.character(data$LOCATIONID)
+    if("LOCATIONID" %in% cols) data$LOCATIONID <- as.integer(data$LOCATIONID)
   }
   return(data)
 }
