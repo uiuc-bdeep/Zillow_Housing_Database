83c871296274eb4309277785b1e3d2d982d4237a
Yuchen Liang <jacksonliang35@gmail.com>
2018-11-26 15:55:47 -0600

    infousa_location

--- BDEEPdb/R/infousa.R
+++ BDEEPdb/R/infousa.R
@@ -47,7 +47,8 @@ get_infousa_location <- function(single_year, fips, tract="*", columns="*", appe
 
   # Process state-county sequentially
   for(i in 1:nrow(state_county)){
-    print(paste("Processing STATE:", toupper(state_county[i, 1]),
+    print(paste("Processing YEAR:", single_year,
+                "STATE:", toupper(state_county[i, 1]),
                 "COUNTY:", state_county[i, 2],
                 "CENSUS2010TRACT:", paste0(tract, collapse = ", ")))
     if(state_county[i, 3] == 0){
@@ -95,12 +96,71 @@ get_infousa_location <- function(single_year, fips, tract="*", columns="*", appe
 #' @import RPostgreSQL DBI
 #' @export
 get_infousa_multiyear <- function(fips, tract="*", columns="*", append=TRUE){
-  res <- get_infousa_location(2006, fips, tract=tract, columns=columns)
-  res$year <- 2006
-  for (yr in 2007:2017){
-    temp <- get_infousa_location(yr, fips, tract=tract, columns=columns)
-    temp$year <- yr
-    res <- rbind(res, temp)
+  # Check valid input
+  if(any(nchar(fips)!=5)){
+    print("Invalid fips codes! Please enter fips code as characters.")
+    return(NULL)
   }
+  state_county <- get_state_county(fips)[, c("state", "county", "county_code")]
+  # state_county$county_code <- as.integer(state_county$county_code)
+  
+  # Initialize tract specification
+  if(length(tract)>1 || tract[1]!="*"){
+    if(nrow(state_county) > 1){
+      print("WARNING: Tracts are unique only in one county!")
+    }
+    tract_spec <- paste0("(\"CENSUS2010TRACT\"=", paste0(tract, collapse = " OR \"CENSUS2010TRACT\"="), ")")
+  }
+
+  # Initialize connection
+  drv <- DBI::dbDriver("PostgreSQL")
+  con <- RPostgreSQL::dbConnect(drv,
+                                dbname = "infousa_2018",
+                                host = "141.142.209.139",
+                                port = 5432,
+                                user = "postgres",
+                                password = "bdeep")
+  
+  first <- True
+  # Iterate over years
+  for(yr in 2006:2017){
+    # Process state-county sequentially
+    for(i in 1:nrow(state_county)){
+      print(paste("Processing YEAR:", single_year,
+                  "STATE:", toupper(state_county[i, 1]),
+                  "COUNTY:", state_county[i, 2],
+                  "CENSUS2010TRACT:", paste0(tract, collapse = ", ")))
+      if(state_county[i, 3] == 0){
+        res_oneyear <- RPostgreSQL::dbGetQuery(con, paste0("SELECT ",
+                                                                paste(columns, collapse = ","),
+                                                                " FROM year", single_year, "part.", state_county[i, 1]))
+      } else if (length(tract)==1 && tract=="*") {
+        res_oneyear <- RPostgreSQL::dbGetQuery(con, paste0("SELECT ",
+                                                           paste(columns, collapse = ","),
+                                                           " FROM year", single_year, "part.", state_county[i, 1],
+                                                           " WHERE \"CENSUS2010COUNTYCODE\"=", state_county[i, 3]))
+      } else {
+        res_oneyear <- RPostgreSQL::dbGetQuery(con, paste0("SELECT ",
+                                                           paste(columns, collapse = ","),
+                                                           " FROM year", single_year, "part.", state_county[i, 1],
+                                                           " WHERE \"CENSUS2010COUNTYCODE\"=", state_county[i, 3],
+                                                           " AND ", tract_spec))
+      }
+      res_oneyear$"YEAR" <- yr
+      gc()
+    }
+    # Append to final result list
+    if(first){
+      res <- res_oneyear
+    } else {
+      res <- rbind(res, res_oneyear)
+    }
+  }
+  
+ 
+  # close the connection
+  RPostgreSQL::dbDisconnect(con)
+  RPostgreSQL::dbUnloadDriver(drv)
+
   return(res)
 }
