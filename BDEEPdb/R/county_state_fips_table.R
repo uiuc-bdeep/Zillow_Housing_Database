e350a38206755a5e57ca43ad097e6dbd68498370
Yuchen Liang <jacksonliang35@gmail.com>
2018-11-26 15:13:50 -0600

    infousa_location

--- BDEEPdb/R/county_state_fips_table.R
+++ BDEEPdb/R/county_state_fips_table.R
@@ -18,20 +18,28 @@
 #'         both in lower case, and third column fips.
 #' @export
 get_state_county <- function(fips){
-  ret <- setNames(data.frame(matrix(ncol = 3, nrow = 0)), c("state", "county", "fips"))
+  ret <- setNames(data.frame(matrix(ncol = 4, nrow = 0)), c("state", "county", "county_code", "fips"))
   for (n in fips){
     # Ensure correct format
-    if(nchar(n)!=5 || n < "01001" || n > "56045"){
+    if(nchar(n)!=5 || n < "01000" || n > "56045"){
       print(paste("FIPS code invalid format:", n))
       next
     }
     # Find row
-    r <- county_state_fips[which(county_state_fips$fips == n), ]
+    if(substr(n, 3, 5) == "000"){
+      # all counties
+      r <- county_state_fips[which(county_state_fips$fips_st == substr(n, 1, 2)), ][1,]
+      r$ct_name <- "ALL"
+      r$fips_ct <- "000"
+      f$fips <- n
+    } else {
+      r <- county_state_fips[which(county_state_fips$fips == n), ]
+    }
     if(nrow(r)!=1){
       print(paste("FIPS code not found:", n))
     }
     # Append
-    ret <- rbind(ret, data.frame(state=r$st_abbr, county=r$ct_name, fips=r$fips))
+    ret <- rbind(ret, data.frame(state=r$st_abbr, county=r$ct_name, county_code=r$fips_ct, fips=r$fips))
   }
   return(ret)
 }
--- BDEEPdb/R/infousa.R
+++ BDEEPdb/R/infousa.R
@@ -1,26 +1,41 @@
 # This file contains functions for data transfer between R and PostgreSQL infousa_2018 database
 # For more information about package PostgreSQL, see https://cran.r-project.org/web/packages/RPostgreSQL/RPostgreSQL.pdf
 
-#' get_from_db_state_city
+#' get_infousa_location
 #' @description This function gets a data.frame including all data from the given state abbr. and county.
-#' @param state_city    A data.frame with two columns representing state and county name
+#' @param year          An integer indicating year
+#' @param fips          A vector of integers indicating fips.
+#'                      If the entire state is wanted, set county code to 000.
+#' @param tract         A vector of integers or chars indicating tract in the county.
+#'                      Note that tracts are unique only in the current county. Default to all tracts.
 #' @param columns       A vector of column names to export. Default to all columns (i.e. "*").
 #' @param append        If append is true, return a single data.frame with rows appended, otherwise a
 #'                      list of data.frames from each state.
-#' @examples table <- data.frame(state=c("IL"),city=c("Chicago"))
-#' @examples data <- get_infousa_state_city(table)
-#' @return A data.frame including all data from the given state and county
+#' @examples  test <- get_infousa_location(2006, 01001)
+#' @examples  test <- get_infousa_location(2006, 02020, tract=c(001802,002900))
+#' @return A data.frame including all data from the given year, fips and tract
 #' @import RPostgreSQL DBI
 #' @export
-get_infousa_state_city <- function(state_city, year, columns="*", append=TRUE){
+get_infousa_location <- function(single_year, fips, tract="*", columns="*", append=TRUE){
   # Check valid input
-  if(nrow(state_city)==0 || ncol(state_city)!=2 || any(nchar(as.character(state_city[,1]))!=2)){
-    print("Invalid argument! Please use this function as followed:")
-    print("get_infousa_state_city(data.frame(state=c(\"IL\"),city=c(\"Chicago\")))")
+  if(any(nchar(fips)!=5)){
+    print("Invalid fips codes!")
     return(NULL)
   }
+  state_county <- get_state_county(fips)[, c("state", "county", "county_code")]
+  # state_county$county_code <- as.integer(state_county$county_code)
+
+  # Initialize tract specification
+  if(tract!="*"){
+    if(nrow(state_county) > 1){
+      print("WARNING: Tracts are unique only in one county!")
+    }
+    tract_spec <- paste0("(\"CENSUS2010TRACT\"=", paste0(tract, collapse = " OR \"CENSUS2010TRACT\"="), ")")
+  }
+  
   # Initialize list for return
   hedonics <- list()
+
   # Initialize connection
   drv <- DBI::dbDriver("PostgreSQL")
   con <- RPostgreSQL::dbConnect(drv,
@@ -29,19 +44,32 @@ get_infousa_state_city <- function(state_city, year, columns="*", append=TRUE){
                                 port = 5432,
                                 user = "postgres",
                                 password = "bdeep")
+
   # Process state-county sequentially
-  for(i in 1:nrow(state_city)){
-    print(paste("Processing state", toupper(state_city[i, 1]), "city", state_city[i, 2]))
-    hedonics[[i]] <- RPostgreSQL::dbGetQuery(con, paste0("SELECT ",
-                                                         paste(columns, collapse = ","),
-                                                         " FROM year", year, " WHERE \"STATE\"='",
-                                                         toupper(state_city[i, 1]), "' AND \"CITY\"='",
-                                                         toupper(state_city[i, 2]), "'"))
+  for(i in 1:nrow(state_county)){
+    print(paste("Processing state", toupper(state_county[i, 1]), "county", state_county[i, 2]))
+    if(state_county[i, 3] == 0){
+      hedonics[[i]] <- RPostgreSQL::dbGetQuery(con, paste0("SELECT ",
+                                                           paste(columns, collapse = ","),
+                                                           " FROM year", single_year, "part.", state_county[i, 1]))
+    } else if (tract=="*") {
+      hedonics[[i]] <- RPostgreSQL::dbGetQuery(con, paste0("SELECT ",
+                                                           paste(columns, collapse = ","),
+                                                           " FROM year", single_year, "part.", state_county[i, 1],
+                                                           " WHERE \"CENSUS2010COUNTYCODE\"=", state_county[i, 3]))
+    } else {
+      hedonics[[i]] <- RPostgreSQL::dbGetQuery(con, paste0("SELECT ",
+                                                           paste(columns, collapse = ","),
+                                                           " FROM year", single_year, "part.", state_county[i, 1],
+                                                           " WHERE \"CENSUS2010COUNTYCODE\"=", state_county[i, 3],
+                                                           " AND ", tract_spec))
+    }
     gc()
   }
-  # Close the connection
+  # close the connection
   RPostgreSQL::dbDisconnect(con)
   RPostgreSQL::dbUnloadDriver(drv)
+
   # Construct returned hedonics
   print("Finished!")
   if(append){
