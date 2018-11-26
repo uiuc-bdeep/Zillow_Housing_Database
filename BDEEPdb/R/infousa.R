06970b40187ed93e95f6897e1174a3073cf89707
Yuchen Liang <jacksonliang35@gmail.com>
2018-11-26 15:46:04 -0600

    infousa_location

--- BDEEPdb/R/infousa.R
+++ BDEEPdb/R/infousa.R
@@ -2,8 +2,8 @@
 # For more information about package PostgreSQL, see https://cran.r-project.org/web/packages/RPostgreSQL/RPostgreSQL.pdf
 
 #' get_infousa_location
-#' @description This function gets a data.frame including all data from the given state abbr. and county.
-#' @param year          An integer indicating year
+#' @description This function gets a data.frame including all data from the given location in a single year.
+#' @param single_year   An integer indicating a single year
 #' @param fips          A vector of integers indicating fips.
 #'                      If the entire state is wanted, set county code to 000.
 #' @param tract         A vector of integers or chars indicating tract in the county.
@@ -34,7 +34,7 @@ get_infousa_location <- function(single_year, fips, tract="*", columns="*", appe
   }
   
   # Initialize list for return
-  hedonics <- list()
+  result <- list()
 
   # Initialize connection
   drv <- DBI::dbDriver("PostgreSQL")
@@ -47,18 +47,20 @@ get_infousa_location <- function(single_year, fips, tract="*", columns="*", appe
 
   # Process state-county sequentially
   for(i in 1:nrow(state_county)){
-    print(paste("Processing state", toupper(state_county[i, 1]), "county", state_county[i, 2], "tract", paste0(tract, collapse = ", ")))
+    print(paste("Processing STATE:", toupper(state_county[i, 1]),
+                "COUNTY:", state_county[i, 2],
+                "CENSUS2010TRACT:", paste0(tract, collapse = ", ")))
     if(state_county[i, 3] == 0){
-      hedonics[[i]] <- RPostgreSQL::dbGetQuery(con, paste0("SELECT ",
+      result[[i]] <- RPostgreSQL::dbGetQuery(con, paste0("SELECT ",
                                                            paste(columns, collapse = ","),
                                                            " FROM year", single_year, "part.", state_county[i, 1]))
     } else if (length(tract)==1 && tract=="*") {
-      hedonics[[i]] <- RPostgreSQL::dbGetQuery(con, paste0("SELECT ",
+      result[[i]] <- RPostgreSQL::dbGetQuery(con, paste0("SELECT ",
                                                            paste(columns, collapse = ","),
                                                            " FROM year", single_year, "part.", state_county[i, 1],
                                                            " WHERE \"CENSUS2010COUNTYCODE\"=", state_county[i, 3]))
     } else {
-      hedonics[[i]] <- RPostgreSQL::dbGetQuery(con, paste0("SELECT ",
+      result[[i]] <- RPostgreSQL::dbGetQuery(con, paste0("SELECT ",
                                                            paste(columns, collapse = ","),
                                                            " FROM year", single_year, "part.", state_county[i, 1],
                                                            " WHERE \"CENSUS2010COUNTYCODE\"=", state_county[i, 3],
@@ -70,11 +72,35 @@ get_infousa_location <- function(single_year, fips, tract="*", columns="*", appe
   RPostgreSQL::dbDisconnect(con)
   RPostgreSQL::dbUnloadDriver(drv)
 
-  # Construct returned hedonics
+  # Construct final result
   print("Finished!")
   if(append){
-    return(do.call("rbind", hedonics))
+    return(do.call("rbind", result))
   } else {
-    return(hedonics)
+    return(result)
   }
+}
+
+
+#' get_infousa_multiyear
+#' @description This function gets a data.frame including all data from the given location from all years.
+#' @param fips          A vector of integers indicating fips.
+#'                      If the entire state is wanted, set county code to 000.
+#' @param tract         A vector of integers or chars indicating tract in the county.
+#'                      Note that tracts are unique only in the current county. Default to all tracts.
+#' @param columns       A vector of column names to export. Default to all columns (i.e. "*").
+#' @examples  test <- get_infousa_location("01001")
+#' @examples  test <- get_infousa_location("02020", tract=c(001802,002900))
+#' @return A data.frame including data from all years, fips and tract
+#' @import RPostgreSQL DBI
+#' @export
+get_infousa_multiyear <- function(fips, tract="*", columns="*", append=TRUE){
+  res <- get_infousa_location(2006, fips, tract=tract, columns=columns)
+  res$year <- 2006
+  for (yr in 2007:2017){
+    temp <- get_infousa_location(yr, fips, tract=tract, columns=columns)
+    temp$year <- yr
+    res <- rbind(res, temp)
+  }
+  return(res)
 }
